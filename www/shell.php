cc9da6e73c3dbf925eb5722c53facb3f5ac53cfa
Masafumi Yamamoto <m-yamamoto@sakura.ad.jp>
2025-05-19 16:40:10 +0900

    update

--- www/shell.php
+++ www/shell.php
@@ -12,7 +12,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 </head>
 <body>
     <form method="post">
-        <input type="text" name="cmd" size="60" autofocus>
+        <input type="text" name="cmd" size="60" autofocus value="<?php echo isset($cmd) ? htmlspecialchars($cmd) : ''; ?>">
         <input type="submit" value="実行">
     </form>
     <pre><?php if (isset($output)) echo htmlspecialchars($output); ?></pre>
